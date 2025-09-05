2f2d7188bbc85782584c4b8188c9f848df7ce4f9
mmaa20007ma-hub <mmaa20007ma@gmail.com>
2025-09-05 17:54:10 +0300

    Add project files.

---
+++ Bootcamp3_AspMVC/Controllers/CategoryController.cs
@@ -0,0 +1,42 @@
+﻿using Bootcamp3_AspMVC.Data;
+using Bootcamp3_AspMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bootcamp3_AspMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = _context.Categories.ToList();
+           
+
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+    }
+}
---
+++ Bootcamp3_AspMVC/Migrations/20250905124013_new.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bootcamp3_AspMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class @new : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Discraption = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
